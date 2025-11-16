9884ba8e4a2447bb0062a379f4194cb3aab5a599
Micfong <micfong2@outlook.com>
2025-11-16 12:57:52 +0000

    fix: change desc

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'GuardAIn',
-  description: 'AI-Powered Personal Safety Assistant',
+  description: 'Your AI Companion for Safer Journeys',
 }
 
 export default function RootLayout({
